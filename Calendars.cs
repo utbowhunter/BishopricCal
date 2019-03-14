bcab30aa64f4e9d34a09be2833d15926e9815e77
utbowhunter <utbowhunter@yahoo.com>
2019-03-14 12:15:06 -0600

    Save stuff

---
+++ Calendars.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bishopric6Cal
+{
+    public static class Calendars
+    {
+        private static List<string> skippedCalendars = new List<string>();
+        private static List<string> calendars = new List<string>();
+
+        public static List<string> SkippedCalendars { get => skippedCalendars; set => skippedCalendars = value; }
+        public static List<string> CalendarList { get => calendars; set => calendars = value; }
+
+        public static void GetCalendars()
+        {
+            List<string> calenders = new List<string>();
+
+            string[] cals = ConfigurationManager.AppSettings["Calendars"].Split(',');
+
+            foreach (string cal in cals)
+            {
+                CalendarList.Add(cal);
+            }
+
+
+
+        }
+        public static void GetSkippedCalendars()
+        {
+            List<string> calenders = new List<string>();
+
+            string[] skippedCals = ConfigurationManager.AppSettings["SkippedCalendars"].Split(',');
+
+            foreach (string skippedCal in skippedCals)
+            {
+                SkippedCalendars.Add(skippedCal);
+            }
+
+
+        }
+        public static bool IsCalSkipped(string id)
+        {
+            bool skippedCal = SkippedCalendars.Contains(id);
+
+            return skippedCal;
+        }
+
+    }
+}
---
+++ Email.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bishopric6Cal
+{
+    public static class Email
+    {
+        public static string BuildEmail(List<Appointment> recurringBishopricAppt, List<Appointment> calAppts)
+        {
+          
+            List<Appointment> fullList = recurringBishopricAppt.Concat(calAppts).OrderBy(x => x.StartTime).ToList();
+            
+            string emailMessage = string.Empty;
+
+            foreach (Appointment appt in fullList)
+            {
+                
+                emailMessage += string.Format("{0, -20}{1, -20}\t{2}\n\r", appt.StartTime?.ToString("h:mm tt"), appt.EndTime?.ToString("h:mm tt"), appt.Text);
+            }
+
+
+            Console.WriteLine(emailMessage);
+
+            return emailMessage;
+
+        }
+
+        internal static void SendEmail(string emailMessage)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string ToString(this DateTime? dt, string format)    => dt == null ? "n/a" : ((DateTime)dt).ToString(format);
+
+    }
+}
